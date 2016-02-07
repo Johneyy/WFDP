4be6e4cf74105ea853341cd0d02f7c51129793c4
TrustMeOnceAgain <fifix94@gmail.com>
2016-02-07 15:11:37 +0100

    Dodanie listy wydarzeń ( można wybrać ale to nic nie daje jak narazie ). Dodanie stylów kolorystycznych do styles.xml

--- app/src/main/java/pwr/wfdp/EventsActivity.java
+++ app/src/main/java/pwr/wfdp/EventsActivity.java
@@ -6,10 +6,31 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EventsActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // List view
+    private ListView lv;
+
+    // Listview Adapter
+    ArrayAdapter<String> adapter;
+
+    // Search EditText
+    EditText inputSearch;
+
+
+    // ArrayList for Listview
+    ArrayList<HashMap<String, String>> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +39,43 @@ public class EventsActivity extends BaseActivity
         makeMenu();
         setFullscreen();
 
+        // Listview Data
+        String products[] = {"Wykłady dla piwowarów - 9:30 - 13:30",
+                "Kurs sensoryczny Flavor Active cz. I - 14:00 - 15:30",
+                "Kurs sensoryczny Flavor Active cz. II - 15:30 - 17:00",
+                "Jak rozpoznać dorbre piwo - 15:00 - 17:00",
+                "Konkurs - 'Dobierz piwo do szkła' - 18:00",};
+
+        lv = (ListView) findViewById(R.id.list_view);
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+
+        // Adding items to listview
+        adapter = new ArrayAdapter<String>(this, R.layout.event_list_item, R.id.product_name, products);
+        lv.setAdapter(adapter);
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                EventsActivity.this.adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+
     }
+
+
 
 }

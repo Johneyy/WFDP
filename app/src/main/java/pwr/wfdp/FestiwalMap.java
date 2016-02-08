6987c5c7de9a6dde33f7f043acc73c492c348ff5
kartoFlane <tbachminski@gmail.com>
2016-02-08 16:42:16 +0100

    Zmienione przyciski w popupie na mapce
    - Zmienione id na czytelniejsze

--- app/src/main/java/pwr/wfdp/FestiwalMap.java
+++ app/src/main/java/pwr/wfdp/FestiwalMap.java
@@ -56,7 +56,7 @@ public class FestiwalMap extends BaseActivity {
             }
         });
 
-        Button about_beer = (Button) layoutt.findViewById(R.id.about_beer);
+        Button about_beer = (Button) layoutt.findViewById(R.id.btn_about_beer);
         about_beer.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(getBaseContext(), AboutStand.class);
@@ -95,7 +95,7 @@ public class FestiwalMap extends BaseActivity {
         Point displaySize = new Point();
         display.getSize( displaySize );
 
-        imageDetail = (ImageView)findViewById( R.id.logoGreenBr );
+        imageDetail = (ImageView)findViewById( R.id.festival_map );
 
         Bitmap bmp = BitmapFactory.decodeResource( getResources(), R.drawable.stoiska );
         Point imageSize = new Point( bmp.getWidth(), bmp.getHeight() );

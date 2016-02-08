0a656c02fbdb270a5084dff41e4d30123e3fe818
kartoFlane <tbachminski@gmail.com>
2016-02-08 17:33:56 +0100

    Caly AboutStand jest teraz scrollowalny, zamiast tylko listy piw
    Bug: po otwarciu, AboutStand scrolluje sie na koniec widoku, zamiast na poczatek. Fix pls.

--- app/src/main/java/pwr/wfdp/AboutStand.java
+++ app/src/main/java/pwr/wfdp/AboutStand.java
@@ -2,8 +2,12 @@ package pwr.wfdp;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,17 +27,50 @@ public class AboutStand extends BaseActivity implements NavigationView.OnNavigat
     ArrayList<HashMap<String, String>> productList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_stand);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_about_stand );
         setFullscreen();
         makeMenu();
-        String products[] = {"Rowing Jack", "Crazy Mike", "Sweet Cow", "Ortodox Mild", "Black Hope",
-                "So Far So Dark AleBrowar i Artezan"};
+        String products[] = {
+                "Rowing Jack",
+                "Crazy Mike",
+                "Sweet Cow",
+                "Ortodox Mild",
+                "Black Hope",
+                "So Far So Dark AleBrowar i Artezan"
+        };
 
         lv = (ListView) findViewById(R.id.listView);
 
         adapter = new ArrayAdapter<String>(this, R.layout.beers_list_item, R.id.beer_name, products);
         lv.setAdapter(adapter);
+
+        justifyListViewHeightBasedOnChildren( lv );
+
+        // TODO: Trzeba zrobic zeby scroll view byl ustawiony na poczatek widoku, a nie na koniec
+        ScrollView sv = (ScrollView)findViewById( R.id.about_stand_scrollView );
+        sv.scrollTo( 0, 0 );
+    }
+
+    private void justifyListViewHeightBasedOnChildren (ListView listView) {
+
+        ListAdapter adapter = listView.getAdapter();
+
+        if (adapter == null) {
+            return;
+        }
+        ViewGroup vg = listView;
+        int totalHeight = 0;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, vg);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams par = listView.getLayoutParams();
+        par.height = totalHeight + (listView.getDividerHeight() * (adapter.getCount() - 1));
+        listView.setLayoutParams(par);
+        listView.requestLayout();
     }
 
 }
